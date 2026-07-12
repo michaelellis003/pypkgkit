701139311cf247739c5c97379ccf998d19770984
Michael Ellis <michaelellis003@gmail.com>
2026-07-12 12:58:50 -0500

    feat: harden template guardrails and fix CI matrix generation (#12)
    * feat: harden template guardrails and fix CI matrix generation
    
    * test: cover CLI entry point to satisfy coverage floor

--- {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.project_slug}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -1,6 +1,9 @@
 # Copyright Contributors to the {{ cookiecutter.project_name }} project.
 # SPDX-License-Identifier: Apache-2.0
 
-"""{{ cookiecutter.project_name }} - {{ cookiecutter.project_description }}."""
+"""{{ cookiecutter.project_name }}.
+
+{{ cookiecutter.project_description | wordwrap(76) }}
+"""
 
 __version__ = "{{ cookiecutter.initial_version }}"
